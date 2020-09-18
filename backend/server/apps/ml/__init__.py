1a024b82cfda2e36a7958331812ba8e1146a1a2c
Jaideep Thambi <jaideep@techvantagesystems.com>
2020-09-18 17:56:24 +0400

    add ml code

---
+++ backend/server/apps/ml/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 15:56:46 2020
+
+@author: jaideep
+"""
+
---
+++ backend/server/apps/ml/income_classifier/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 15:58:52 2020
+
+@author: jaideep
+"""
+
